e25c2fd18f682b3211c840dbf3f4be7e288fd582
Joseph <joseph.msltd@gmail.com>
2022-10-17 20:29:41 +0100

    Update PbToGCS_CF.py

--- dataflow/PbToGCS_CF.py
+++ dataflow/PbToGCS_CF.py
@@ -15,7 +15,7 @@ def hello_pubsub(event, context):
     curr_time = round(time.time()*1000)
     filename = str(curr_time)+".json"
     print(filename)
-    bucket_name = 'my-test-bu123'
+    bucket_name = 'pbtogcs_bucket'
     bucket = storage.Client().get_bucket(bucket_name)
     # 'chosen-path-to-object/{name-of-object}'
     blob = bucket.blob('output/'+filename)
